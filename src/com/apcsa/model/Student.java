aded41d0e578a3db2e00bdee7fa60354af6091fe
David Vaykhanskiy <dvaykhanskiy@ucvts.tec.nj.us>
2019-12-11 08:33:32 -0500

    filled out constructor for student

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -6,9 +6,7 @@ import com.apcsa.model.User;
 
 public class Student extends User {
 
-    public Student(User user, ResultSet rs) {
-    	
-	}
+    
 	private int studentId;
     private int classRank;
     private int gradeLevel;
@@ -16,5 +14,17 @@ public class Student extends User {
     private double gpa;
     private String firstName;
     private String lastName;
+    
+    public Student(User user, ResultSet rs) {
+    	super(user.getUserId(), user.getAccountType(), user.getUsername(), user.getPassword(), user.getLastLogin());
+    	
+    	this.studentId = rs.getInt("student_id");
+    	this.classRank = rs.getInt("class_rank");
+    	this.gradeLevel = rs.getInt("grade_level");
+    	this.graduationYear = rs.getInt("graduation_year");
+    	this.gpa = rs.getDouble("gpa");
+    	this.firstName = rs.getString("first_name");
+    	this.lastName = rs.getString("last_name");
+	}
     
 }
