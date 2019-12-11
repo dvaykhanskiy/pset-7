af911b59e2db8c5c30631f747628ecd2dfa50552
David Vaykhanskiy <dvaykhanskiy@ucvts.tec.nj.us>
2019-12-11 08:23:01 -0500

    Created student and teacher constructors

--- src/com/apcsa/model/Student.java
+++ src/com/apcsa/model/Student.java
@@ -1,10 +1,15 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
 import com.apcsa.model.User;
 
 public class Student extends User {
 
-    private int studentId;
+    public Student(User user, ResultSet rs) {
+    	
+	}
+	private int studentId;
     private int classRank;
     private int gradeLevel;
     private int graduationYear;
--- src/com/apcsa/model/Teacher.java
+++ src/com/apcsa/model/Teacher.java
@@ -1,10 +1,15 @@
 package com.apcsa.model;
 
+import java.sql.ResultSet;
+
 import com.apcsa.model.User;
 
 public class Teacher extends User {
 
-    private int teacherId;
+    public Teacher(User user, ResultSet rs) {
+		
+	}
+	private int teacherId;
     private int departmentId;
     private String firstName;
     private String lastName;
