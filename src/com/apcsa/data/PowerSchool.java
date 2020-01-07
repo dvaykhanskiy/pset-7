58aed2aa9f1eaca63366e1da451cf9ae4749b2bf
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-01-06 21:03:50 -0500

    added confirmation function

--- src/com/apcsa/data/PowerSchool.java
+++ src/com/apcsa/data/PowerSchool.java
@@ -364,5 +364,26 @@ public class PowerSchool {
          }
      }
      
+     /**
+      * Confirms a user's intent to perform an action.
+      * 
+      * @param in the Scanner
+      * @param message the confirmation prompt
+      * @return true if the user confirms; false otherwise
+      */
+
+     public static boolean confirm(Scanner in, String message) {
+         String response = "";
+         
+         // prompt user for explicit response of yes or no
+         
+         while (!response.equals("y") && !response.equals("n")) {
+             System.out.print(message);
+             response = in.next().toLowerCase();
+         }
+         
+         return response.equals("y");
+     }
+     
      
 }
