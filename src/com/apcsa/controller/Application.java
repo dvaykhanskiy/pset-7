7b41b6a8643543a4d25f1917a6c7f92941601025
dvaykhanskiy <dvaykhanskiy@ucvts.org>
2020-01-11 23:20:39 -0500

    completed reset password function

--- src/com/apcsa/controller/Application.java
+++ src/com/apcsa/controller/Application.java
@@ -424,15 +424,18 @@ public class Application {
      }
     
     private void resetPassword() {
-        //
-        // prompt root user to enter username of user whose password needs to be reset
-        //
-        // ask root user to confirm intent to reset the password for that username
-        //
-        // if confirmed...
-        //      call database method to reset password for username
-        //      print success message
-        //
+    	System.out.print("\nUsername: ");
+        String username = in.next();
+        if (Utils.confirm(in, "\nAre you sure you want to reset the password for " + username + "?  (y/n) ")) {
+            if (in != null) {
+                if (PowerSchool.resetPassword(username)) {
+                    PowerSchool.resetLastLogin(username);
+                    System.out.println("\nSuccessfully reset password for " + username + ".");
+                } else {
+                    System.out.println("\nPassword reset failed");
+                }
+            }
+        }
     }
     
     private void factoryReset() {
